60422998424886594c6a8725ed0cdab51b62ea84
Neeharika Garg <Neeharika.Garg@technoforte.co.in>
2021-12-24 13:16:02 +0530

    admin automation

---
+++ automation/src/main/java/automation/utility/SetTestName.java
@@ -0,0 +1,9 @@
+package automation.utility;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface SetTestName {
+    int idx() default 0;
+}

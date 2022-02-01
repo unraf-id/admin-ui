2a5e15074fd47db91aa6a400890bdd0af325e9db
Neeharika Garg <Neeharika.Garg@technoforte.co.in>
2022-02-01 12:14:44 +0530

    Folder group id

--- automation/src/main/java/automation/utility/SetTestName.java
+++
@@ -1,9 +0,0 @@
-package automation.utility;
-
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-
-@Retention(RetentionPolicy.RUNTIME)
-public @interface SetTestName {
-    int idx() default 0;
-}
